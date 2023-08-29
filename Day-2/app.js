7fa4b41583fcca4dd7e420efdef31a6b10b44ec6
anis520 <anisulhoque587@gmail.com>
2023-08-29 08:44:09 +0600

    done day3

--- Day-2/app.js
+++ Day-2/app.js
@@ -1,1 +1,17 @@
 // Your JavaScript code here
+// Day 02
+// write a function that returns the sum of two numvers
+//  write a funtion that returns the sum of all numbers regardless of   params
+
+console.log("day 02");
+
+const totalSum = (...parms) => {
+  let total = 0;
+  parms.map((element) => {
+    total += element;
+  });
+
+  return total;
+};
+
+console.log(totalSum(4, 6, 85));
--- Day-3/app.js
+++ Day-3/app.js
@@ -1,1 +1,18 @@
 // Your JavaScript code here
+// Give a rectangular matrix of characters ,add a border of asterisks (*) to it .
+
+console.log("day 03");
+
+const myfunction = (array) => {
+  const makeWall = "*".repeat(array[0].length + 2);
+  array.unshift(makeWall);
+  array.push(makeWall);
+
+  for (let i = 0; i < array.length; i++) {
+    array[i] = "*".concat(array[i], "*");
+  }
+
+  console.log(array);
+};
+
+myfunction(["abid", "anis"]);
